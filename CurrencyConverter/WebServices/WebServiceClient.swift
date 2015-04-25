15032f1ca027314456b7cab14792bb65e3d99a7a
Jaikumar Bhambhwani <jai.bhambhwani@gmail.com>
2015-04-25 16:17:51 -0700

    Added json for spoofer

---
+++ CurrencyConverter/WebServices/WebServiceClient.swift
@@ -0,0 +1,9 @@
+//
+//  WebServiceClient.swift
+//  CurrencyConverter
+//
+//  Created by Jaikumar Bhambhwani on 4/25/15.
+//  Copyright (c) 2015 Jaikumar Bhambhwani. All rights reserved.
+//
+
+import Foundation
