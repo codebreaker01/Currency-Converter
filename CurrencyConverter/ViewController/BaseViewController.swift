de42a1385ddc41ad886e6b081548cd2e0772dad4
Jaikumar Bhambhwani <jai.bhambhwani@gmail.com>
2015-05-22 22:01:53 -0700

    Added background gradient

--- CurrencyConverter/ViewController/BaseViewController.swift
+++ CurrencyConverter/ViewController/BaseViewController.swift
@@ -12,10 +12,20 @@ class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.view.bounds
+        gradient.colors = [UIColor.themeDarkBlue().CGColor, UIColor.themeDarkBlueTint().CGColor]
+        self.view.layer.insertSublayer(gradient, atIndex: 0)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
 }
 
